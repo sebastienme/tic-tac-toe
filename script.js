6a71582e5cd24e8e57e95ecff2736b18a16f5767
sebastien <sebastien.meslage@gmail.com>
2023-04-22 16:39:36 -0400

    ...

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+const Player = (token) => {
+   
+    const getToken = () => token;
+
+    return {getToken};
+}
+
+
 const gameBoard = (() => {
     
     const board = new Array(9);
 
-    const boardLenght = () => board.length;
+    //initialise the two players and starts the game
+    const createPlayers = () => {
+        const player1 = Player('x');
+        const player2 = Player('o');
+    }
 
-    return {boardLenght};
+    // Event listeners 
+    const playBtn = document.getElementById('start-button');
+    playBtn.addEventListener('click', createPlayers);
+
+    return {};
 })();
 
+
 const displayController = (() => {
-    
+
     const displayBoard = (number) => {
         for (let i = 0; i < number; i++) {
            domElements.append('.tic-tac-grid', domElements.div("case", i))
@@ -18,8 +35,10 @@ const displayController = (() => {
     return {displayBoard}
 })();
 
-const domElements = (() => {
 
+const domElements = (() => {
+    
+    //create a div element
     const div = (classe, id) => {
         const newDiv = document.createElement("div");
         newDiv.setAttribute('class', classe)
@@ -27,12 +46,22 @@ const domElements = (() => {
         return newDiv;
     }
 
+    //append an element
     const append = (selector, toAppend) => {
         const element = document.querySelector(selector);
         element.appendChild(toAppend);
     }
 
-    return {div, append};
+    //select an element
+    const selector = (element) => document.querySelector(element);
+
+    //select elements
+    const selectors = (element) => document.querySelectorAll(element);
+
+
+
+    return {div, append, selector, selectors};
 })();
 
-displayController.displayBoard(gameBoard.boardLenght());
+
+displayController.displayBoard(9);
